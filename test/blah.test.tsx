9bf13c7c6432b48d91b41f5eecc88bbae07412e1
daviddexteel <dsilvestri@dexteel.com>
2023-12-03 19:13:23 -0300

    first commit

---
+++ test/blah.test.tsx
@@ -0,0 +1,13 @@
+// import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+
+describe('it', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    // ReactDOM.render(<Thing />, div);
+    ReactDOM.unmountComponentAtNode(div);
+    console.log("hello world 2");
+
+  });
+});
